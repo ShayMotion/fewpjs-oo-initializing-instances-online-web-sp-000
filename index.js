ced346b9038fb19fbb2b05fed0eba11f258f7102
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-20 18:35:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let newLunch = new Lunch;
 }
 
 class Dinner {
-  constructor(salad, soup, entree, dessert);
-  
+  constructor(salad, soup, entree, dessert);{
+  this.salad = salad;
+  this.soup = soup;
+  this.entree = entree;
+  this._dessert = dessert;
+  }
 }
