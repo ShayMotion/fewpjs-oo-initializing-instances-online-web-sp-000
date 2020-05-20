99d7fc5d6e3e0a78347e36e53664005e01d10852
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-20 18:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ constructor(food, drink){
 
 class Lunch {
   constructor(salad, soup, drink){
-let newLunch = new Lunch();
 this.salad = salad;
 this.soup = soup;
 this.drink = drink;
   }
 }
 
-// class Dinner {
-//   constructor(salad, soup, entree, dessert);{
-//   this.salad = salad;
-//   this.soup = soup;
-//   this.entree = entree;
-//   this._dessert = dessert;
-//   }
-// }
+class Dinner {
+  constructor(salad, soup, entree, dessert);{
+  this.salad = salad;
+  this.soup = soup;
+  this.entree = entree;
+  this._dessert = dessert;
+  }
+}
